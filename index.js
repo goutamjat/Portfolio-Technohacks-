5f51558926c5b857ae934769849d214052f2269f
Goutam Jat <goutamjat12@gmail.com>
2024-08-14 23:30:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new typed(".text", {
+    Strings:["Full Stack Developer", "Web Developer", "Frontend Developer","Backend Developer", "Wordpress Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay:1000,
+    loop:true
+});
